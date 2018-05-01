44df3688afe00cd6ed7d7e1ad5dd8b82bb33f686
mels_gbkim <melsgbkim@gmail.com>
2018-05-01 19:51:10 +0900

    Add Shadow

--- Assets/Script/Enemy/EnemyInfo.cs
+++ Assets/Script/Enemy/EnemyInfo.cs
@@ -20,14 +20,50 @@ public class EnemyInfo : MonoBehaviour
             {
                 _id = value;
 
-                GetComponent<Animator>().runtimeAnimatorController = Resources.Load(XMLUtil.FindOneByTag(EnemyNode, "Animation").InnerText) as RuntimeAnimatorController;
                 GetComponent<Rigidbody2D>().mass = float.Parse(XMLUtil.FindOneByTag(EnemyNode, "RigidbodyMass").InnerText);
                 GetComponent<Rigidbody2D>().drag = float.Parse(XMLUtil.FindOneByTag(EnemyNode, "RigidbodyDrag").InnerText);
                 gameObject.AddComponent<EnemyLogic>();
                 gameObject.AddComponent<EnemyCollisionHandler>();
+
+                SetChildInfo();
             }
         }
     }
+
+    void SetChildInfo()
+    {
+        for (int i = 0; i < transform.GetChildCount(); i++)
+        {
+            Transform child = transform.GetChild(i);
+            XmlNodeList list = EnemyNode.GetElementsByTagName(child.name);
+            if (list.Count == 0) continue;
+            foreach (XmlElement node in list[0].ChildNodes)
+            {
+                string val = node.InnerText;
+                try
+                {
+                    switch (node.Name)
+                    {
+                        case "x": SetChildLocalPosX(child, val);break;
+                        case "y": SetChildLocalPosY(child, val);break;
+                        case "z": SetChildLocalPosZ(child, val);break;
+                        case "scale": SetChildLocalScale(child,val); break;
+                        case "Animation": SetChildSpriteAnimation(child,val); break;
+                    }
+                }
+                catch (System.Exception e) { }
+            }
+
+        }
+    }
+    void SetChildLocalPosX(Transform c, string value) { c.localPosition = new Vector3(float.Parse(value), c.localPosition.y,  c.localPosition.z); }
+    void SetChildLocalPosY(Transform c, string value) { c.localPosition = new Vector3(c.localPosition.x,  float.Parse(value), c.localPosition.z); }
+    void SetChildLocalPosZ(Transform c, string value) { c.localPosition = new Vector3(c.localPosition.x, c.localPosition.y, float.Parse(value)); }
+    void SetChildLocalScale(Transform c, string value) { c.localScale = new Vector3(float.Parse(value), float.Parse(value), 0); }
+    void SetChildSpriteAnimation(Transform c, string value) {
+        Animator tmp = c.GetComponent<Animator>();
+        tmp.runtimeAnimatorController = Resources.Load(value) as RuntimeAnimatorController; }
+
     public void OnDead()
     {
         XmlElement DestroyNode = XMLUtil.FindOneByTag(EnemyNode, "Destroy");
---
+++ Assets/Script/ShadowPositionUpdater.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShadowPositionUpdater : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v = transform.localPosition;
+        transform.localPosition = new Vector3(v.x, v.y, -transform.parent.localPosition.z);
+    }
+}
