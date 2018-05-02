d2454c739f2cad6645146b920f0460583655c329
mels_gbkim <melsgbkim@gmail.com>
2018-05-02 23:58:26 +0900

    UI button click

---
+++ Assets/Script/Main/MainUiToggleManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainUiToggleManager : MonoBehaviour
+{
+    bool UIStat = false;
+    bool UISkill = false;
+    bool UIInven = false;
+    bool UIUpgrade = false;
+    bool UIQuest = false;
+    bool UIOption = false;
+
+    public void ToggleUIStat() { UIStat = !UIStat; }
+    public void ToggleUISkill() { UISkill = !UISkill; }
+    public void ToggleUIInven() { UIInven = !UIInven; }
+    public void ToggleUIUpgrade() { UIUpgrade = !UIUpgrade; }
+    public void ToggleUIQuest() { UIQuest = !UIQuest; }
+    public void ToggleUIOption() { UIOption = !UIOption; }
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(UIStat + " " +
+             UISkill + " " +
+             UIInven + " " +
+              UIUpgrade + " " +
+              UIQuest + " " +
+              UIOption + " ");
+    }
+}
