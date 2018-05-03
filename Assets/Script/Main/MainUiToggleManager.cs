24e875a1ab8bc1df5b416947709814ee777d659b
mels_gbkim <melsgbkim@gmail.com>
2018-05-03 23:51:14 +0900

    Create Temp Inventory

--- Assets/Script/Main/MainUiToggleManager.cs
+++ Assets/Script/Main/MainUiToggleManager.cs
@@ -1,21 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MainUiToggleManager : MonoBehaviour
 {
-    bool UIStat = false;
-    bool UISkill = false;
-    bool UIInven = false;
-    bool UIUpgrade = false;
-    bool UIQuest = false;
-    bool UIOption = false;
+    string _NowOpenedUI = "";
+    public string NowOpenedUI
+    {
+        get { return _NowOpenedUI; }
+        set
+        {
+            for(int i=0;i<UIList.Count;i++)
+            {
+                if (UIList[i].name == _NowOpenedUI || UIList[i].name == value)
+                {
+                    if (UIList[i].end)
+                        UIUpdateList.Add(UIList[i]);
+                    UIList[i].toggle = (UIList[i].name == value);
+                }
+            }
+            _NowOpenedUI = value;
+        }
+    }
 
-    public void ToggleUIStat() { UIStat = !UIStat; }
-    public void ToggleUISkill() { UISkill = !UISkill; }
-    public void ToggleUIInven() { UIInven = !UIInven; }
-    public void ToggleUIUpgrade() { UIUpgrade = !UIUpgrade; }
-    public void ToggleUIQuest() { UIQuest = !UIQuest; }
-    public void ToggleUIOption() { UIOption = !UIOption; }
+    public void ToggleUIStat()      { NowOpenedUI="Stat";}
+    public void ToggleUISkill()     { NowOpenedUI="Skill";}
+    public void ToggleUIInven()     { NowOpenedUI="Inven";}
+    public void ToggleUIUpgrade()   { NowOpenedUI="Upgrade";}
+    public void ToggleUIQuest()     { NowOpenedUI="Quest";}
+    public void ToggleUIOption()    { NowOpenedUI="Option";}
+
+    public List<UIPositionUpdater> UIList = new List<UIPositionUpdater>();
+    List<UIPositionUpdater> UIUpdateList = new List<UIPositionUpdater>();
+
 
 
     // Use this for initialization
@@ -27,11 +44,16 @@ public class MainUiToggleManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print(UIStat + " " +
-             UISkill + " " +
-             UIInven + " " +
-              UIUpgrade + " " +
-              UIQuest + " " +
-              UIOption + " ");
+        for (int i = 0; i < UIUpdateList.Count; i++)
+            UIUpdateList[i].UpdatePos();
+        for (int i = 0; i < UIUpdateList.Count; )
+        {
+            if (UIUpdateList[i].end)
+            {
+                UIUpdateList.Remove(UIUpdateList[i]);
+            }
+            else
+                i++;
+        }
     }
 }
---
+++ Assets/Script/UI/UIPositionUpdater.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIPositionUpdater : MonoBehaviour
+{
+    bool _toggle = false;
+    float progress = 0f;
+    public bool end = true;
+    public bool toggle
+    {
+        get
+        {
+            return _toggle;
+        }
+        set
+        {
+            if(_toggle != value)
+            {
+                end = false;
+                _toggle = value;
+                progress = 0f;
+            }
+        }
+    }
+
+
+    public string name = "";
+    public Vector3 PosToggleOFF;
+    public Vector2 SizeToggleOFF = Vector2.one;
+    public Vector3 PosToggleON;
+    public Vector2 SizeToggleON = Vector2.one;
+    public float SpeedToggle = 2f;
+    public float SpeedTogglePow = 1f;
+    // Use this for initialization
+    void Start()
+    {
+        transform.localPosition = PosToggleOFF;
+    }
+
+    public void UpdatePos()
+    {
+        progress += SpeedToggle * Time.deltaTime;
+
+        if (progress > 1f) end = true;
+        if (progress > 1f) progress = 1f;
+
+        float per = 0f;
+        if (toggle) per = 1-Mathf.Pow(1-progress, SpeedTogglePow);
+        else        per = 1-Mathf.Pow(progress, SpeedTogglePow);
+
+
+        transform.localPosition = per * PosToggleON + (1 - per) * PosToggleOFF;
+        transform.localScale = per * SizeToggleON + (1 - per) * SizeToggleOFF;
+       
+    }
+}
